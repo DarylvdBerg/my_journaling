b1fc7ab5eaa13c47b9868fe75c156dac96a8e875
daryl <darylvdberg@gmail.com>
2020-01-31 15:20:17 +0100

    Update to ListTile styling.

--- lib/widgets/journal_list.dart
+++ lib/widgets/journal_list.dart
@@ -1,15 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:my_journaling/models/journal.dart';
 
 class JournalList extends StatelessWidget {
-//  final AsyncSnapshot snapshot;
   final List<Journal> journals;
 
   JournalList({this.journals});
 
 
-  String _formatDate(Timestamp timestamp) {
+  String _getDayFromTimestamp(Timestamp timestamp) {
+    DateTime date = timestamp.toDate();
+    return DateFormat('EEEE').format(date);
+  }
+
+  String _getFullDateFromTimestamp(Timestamp timestamp) {
+    DateTime date = timestamp.toDate();
+    return DateFormat('dd-MM-yyyy HH:mm').format(date);
   }
 
   @override
@@ -19,12 +26,16 @@ class JournalList extends StatelessWidget {
       shrinkWrap: true,
       itemCount: journals.length,
       itemBuilder: (BuildContext context, int index) {
-        return new Column(
-          children: <Widget>[
-            new ListTile(
-              title: Text(journals[index].journal),
+        return Container(
+          child: Card(
+            child: ListTile(
+              leading: Icon(Icons.border_color),
+              title: Text(_getDayFromTimestamp(journals[index].date)),
+              subtitle: Text(_getFullDateFromTimestamp(journals[index].date)),
+              onTap: () {
+              },
             )
-          ],
+          ),
         );
       },
     );
